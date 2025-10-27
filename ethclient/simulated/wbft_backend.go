99396eb0e8b872d2b29ea284d39002b81b696ffa
ian(WM) <ian.jungyong.um@gmail.com>
2025-10-27 11:29:30 +0900

    test: apply base fee policy to WBFT simulation and test

--- ethclient/simulated/wbft_backend.go
+++ ethclient/simulated/wbft_backend.go
@@ -55,7 +55,7 @@ func NewWBFTBackend(alloc types.GenesisAlloc, options ...func(nodeConf *node.Con
 		GasLimit:   ethconfig.Defaults.Miner.GasCeil,
 		Alloc:      alloc,
 		Difficulty: new(big.Int).SetUint64(1),
-		BaseFee:    big.NewInt(1000000000),
+		BaseFee:    new(big.Int).SetUint64(params.MinBaseFee),
 	}
 	validator := crypto.PubkeyToAddress(nodeConf.P2P.PrivateKey.PublicKey)
 	blsKey, _ := bls.DeriveFromECDSA(nodeConf.P2P.PrivateKey)
