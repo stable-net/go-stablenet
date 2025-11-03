ec6a2b4f4df9de2b6b396eef0a0cdc17c2e20a4a
mhha(WM) <mhha@wemade.com>
2025-11-03 17:57:50 +0900

    chore: update license headers in systemcontracts
    - Replace go-wemix-wbft with go-stablenet
    - Add full GPL license text to all files
    - 19 files updated in systemcontracts directory

--- systemcontracts/test/utils.go
+++ systemcontracts/test/utils.go
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright 2025 The go-wemix-wbft Authors
-// This file is part of the go-wemix-wbft library.
+// Copyright 2025 The go-stablenet Authors
+// This file is part of the go-stablenet library.
 //
-// The go-wemix-wbft library is free software: you can redistribute it and/or modify
+// The go-stablenet library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-wemix-wbft library is distributed in the hope that it will be useful,
+// The go-stablenet library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the go-wemix-wbft library. If not, see <http://www.gnu.org/licenses/>.
+// along with the go-stablenet library. If not, see <http://www.gnu.org/licenses/>.
 
 package test
 
