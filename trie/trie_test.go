c6dff3322ee761d548b44f7fff5c9e68a409f209
colinkim(wm) <kim.cy@outlook.com>
2026-04-02 11:24:58 +0900

    chore: chagne lib

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -898,7 +898,7 @@ func TestCommitSequence(t *testing.T) {
 	} {
 		addresses, accounts := makeAccountsWithExtra(tc.count, false)
 		// This spongeDb is used to check the sequence of disk-db-writes
-		s := &spongeDb{sponge: sha3.NewLegacyKeccak256()}
+		s := &spongeDb{sponge: crypto.NewKeccakState()}
 		db := newTestDatabase(rawdb.NewDatabase(s), rawdb.HashScheme)
 
 		// Fill the trie with elements
