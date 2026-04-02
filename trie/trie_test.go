bc2864c728c27e1f8111a080b314832feb90e36d
colinkim(wm) <kim.cy@outlook.com>
2026-04-02 13:41:08 +0900

    chore: reverted the changes to the library

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -898,7 +898,7 @@ func TestCommitSequence(t *testing.T) {
 	} {
 		addresses, accounts := makeAccountsWithExtra(tc.count, false)
 		// This spongeDb is used to check the sequence of disk-db-writes
-		s := &spongeDb{sponge: crypto.NewKeccakState()}
+		s := &spongeDb{sponge: sha3.NewLegacyKeccak256()}
 		db := newTestDatabase(rawdb.NewDatabase(s), rawdb.HashScheme)
 
 		// Fill the trie with elements
@@ -932,7 +932,8 @@ func TestCommitSequenceRandomBlobs(t *testing.T) {
 	} {
 		// This spongeDb is used to check the sequence of disk-db-writes
 		prng := rand.New(rand.NewSource(int64(i)))
-		s := &spongeDb{sponge: crypto.NewKeccakState()}
+		// This spongeDb is used to check the sequence of disk-db-writes
+		s := &spongeDb{sponge: sha3.NewLegacyKeccak256()}
 		db := newTestDatabase(rawdb.NewDatabase(s), rawdb.HashScheme)
 
 		// Fill the trie with elements
